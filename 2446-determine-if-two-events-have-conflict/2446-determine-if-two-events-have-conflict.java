02d26882a3dfa42c3e9358d1b52cadf8e95c1900
Tanmoy Mahato <tanmoy.mato@gmail.com>
2022-11-22 21:23:12 +0530

    Delete 2446-determine-if-two-events-have-conflict directory

--- 2446-determine-if-two-events-have-conflict/2446-determine-if-two-events-have-conflict.java
+++
@@ -1,9 +0,0 @@
-class Solution {
-    public boolean haveConflict(String[] event1, String[] event2) {
-        
-      if(event1[0].compareTo(event2[1])<=0 && event2[0].compareTo(event1[1])<=0)
-          return true;
-      else 
-          return false;
-    }
-}
